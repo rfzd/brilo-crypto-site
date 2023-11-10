e425702a600d541c3991417f6f134e1537e2581b
Petr Vizina <petr@autix.eu>
2023-11-11 00:28:10 +0100

    Change indent to spaces

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ require_once sprintf('%s/vendor/autoload_runtime.php', dirname(path: __DIR__));
 
 return static function (array $context): Kernel {
     return new Kernel(
-		environment: $context['APP_ENV'],
-		debug: (bool) $context['APP_DEBUG'],
-	);
+        environment: $context['APP_ENV'],
+        debug: (bool) $context['APP_DEBUG'],
+    );
 };
